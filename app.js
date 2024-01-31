97fd66cf72ffd1db146b3bff002ac323f686c7be
adityadixit29 <118058546+adityadixit29@users.noreply.github.com>
2024-01-31 16:09:03 +0530

    Add files via upload
    initial-commit

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import userRouter from "./routes/user.js"
+export const app = express();
+import {config} from "dotenv"
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middleware/error.js";
+import cors from "cors";
+
+config()
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+      origin: [process.env.FRONTED_URL],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
+//routes
+app.use("/api/v1/users", userRouter)
+
+
+//using error middleware
+app.use(errorMiddleware);
+
+app.get('/',(req,res)=>{
+    res.send("running")
+})
+
