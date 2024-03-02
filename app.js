442db9afd007b7320984b24db6a69752fc49d6e7
adityadixit29 <118058546+adityadixit29@users.noreply.github.com>
2024-03-02 20:37:37 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -8,13 +8,6 @@ import cors from "cors";
 
 config()
 //middlewares
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTED_URL);
-
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
 
 app.use(express.json());
 app.use(cookieParser());
